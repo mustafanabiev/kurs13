b65b6b508dd234558be11668114d478318fb62e6
mustafanabiev <nabievvv02@gmail.com>
2023-05-23 00:29:09 +0600

    class

---
+++ dart_lessons/sabak_07/bin/sabak_07.dart
@@ -0,0 +1,23 @@
+void main() {
+  Olko kg = Olko();
+
+  print(kg.name);
+  print(Olko().kalk);
+}
+
+// Mers
+String text = 'Salam';
+
+// BMW
+int san = 10;
+
+// Tiko
+bool ozgormo2 = true;
+
+class Olko {
+  String name = 'Kyrgyzstan';
+  int kalk = 7000000;
+  String tili = 'Kyrgyz';
+  int aimak = 200000;
+  bool egemenduubu = true;
+}
---
+++ dart_lessons/sabak_07/test/sabak_07_test.dart
@@ -0,0 +1,8 @@
+import 'package:sabak_07/sabak_07.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
