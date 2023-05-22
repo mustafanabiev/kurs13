46735b4e9d6059b91c4ce51fb81fa576dc9d608c
mustafanabiev <nabievvv02@gmail.com>
2023-05-22 23:53:21 +0600

    bmi

--- dart_lessons/sabak06/test/sabak06_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sabak06/sabak06.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- quiz_app2/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:quiz_app2/app/app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
