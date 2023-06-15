98d175366058cba256543f50767ccad80da38145
mustafanabiev <nabievvv02@gmail.com>
2023-06-16 00:33:58 +0600

    comment buttu

--- comment_project/lib/home_page.dart
+++ comment_project/lib/home_page.dart
@@ -1,4 +1,5 @@
 const int sanBir = 10;
+
 String chonTextter = 'Salam';
 
 class MeninClassym {}
@@ -6,3 +7,27 @@ class MeninClassym {}
 var a = MeninClassym();
 
 const List b = [1, 2, 3, 4];
+
+void koshuu() {
+  int a = 2 + 4;
+  print(a);
+}
+
+int san() {
+  int b = 60;
+  return b;
+}
+
+List<int> sandar = [1, 2, 3, 4, 5];
+
+// int: sandin tibi
+// san: ozgormonyn aty.
+// 0: ozgormonyn maanisi
+int san1 = 0;
+
+String text() {
+  String a = 'Salam';
+  return a;
+}
+
+String text2() => 'Salam';
--- comment_project/lib/main.dart
+++ comment_project/lib/main.dart
@@ -6,9 +6,9 @@ import 'package:flutter/material.dart';
 // (): main funksiasynyn parametri
 // {}: main funksiasynyn denesi
 void main() {
-// runApp: Tirkemeni jurguzup beret
-// const: ozgorboi torgan nersege berebis
-// MyApp(): classtyn aty
+  // runApp: Tirkemeni jurguzup beret
+  // const: ozgorboi torgan nersege berebis
+  // MyApp(): classtyn aty
   runApp(const MyApp());
 }
 
@@ -20,62 +20,128 @@ class MyApp extends StatelessWidget {
   // super: child classtan parent classka otkoryn beret. MyApp ---> StatelessWidget
   const MyApp({super.key});
 
-// @override: parent classtagy method du child classta ozgortup jazuu. MyApp <--- StatelessWidget
+  // @override: parent classtagy method du child classta ozgortup jazuu. MyApp <--- StatelessWidget
   @override
+  // Widget: build methodunun kaitaruu tibi
+  // build: methoddun aty
+  // (BuildContext context): build methoddunun parametri
+  // {}: build methodunun denesi
   Widget build(BuildContext context) {
+    // return: kaitarip beret
+    // MaterialApp: bul bir -> Widget
     return MaterialApp(
+      // title: MaterialApp proportisi. Ozino String alat.
       title: 'Flutter Demo',
+      // theme: MaterialApp proportisi. Ozino ThemeData alat
+      // ThemeData: bul -> Widget
       theme: ThemeData(
+        // colorScheme: ThemeData proportisi. Ozino ColorScheme alat
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // useMaterial3: ThemeData proportisi. Ozino bool alat
         useMaterial3: true,
       ),
+      // home: MaterialApp proportisi. Ozino Widget alat
+      // MyHomePage: bul bir Widget
+      // title: MyHomePage proportisi. Ozino Strign alat
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+// class: keyword, classtyn dartka bir tushunuk kirgize alabys
+// MyHomePage: classtyn aty
+// extends: muras alat. MyApp <--- StatefulWidget
 class MyHomePage extends StatefulWidget {
+  // MyHomePage({}): constructor.
+  // super: child classtan parent classka otkoryn beret. MyApp ---> StatefulWidget
+  // required: sozsuz turdo beruu kerek
   const MyHomePage({super.key, required this.title});
-
+  // final: ozgorboit
+  // String: title proportinin tibi
+  // title: proportinin aty
   final String title;
-
+  // @override: parent classtagy method du child classta ozgortup jazuu. MyApp <--- StatefulWidget
   @override
+  // State: abal
+  // <MyHomePage>: MyHomePage turun berebis
+  // createState(): methoddun aty
+  // _MyHomePageState: classtun aty
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// class: keyword, classtyn dartka bir tushunuk kirgize alabys
+// _MyHomePageState: classtun aty
+// extends: muras alat. MyApp <--- State<MyHomePage>
 class _MyHomePageState extends State<MyHomePage> {
+  // int: _counterdin tibi
+  // _counter: ozgormonun aty
+  // 0: ozgormonun maanisi
   int _counter = 0;
-
+  // void: _incrementCounterdin tibi
+  // _incrementCounter: methoddun aty
+  // (): _incrementCounterdin parameri
+  // {}: _incrementCounterdin denesi
   void _incrementCounter() {
+    // setState: method
     setState(() {
+      // _counter: ozgormonun aty
       _counter++;
     });
   }
 
+  // @override: parent classtagy method du child classta ozgortup jazuu. MyApp <--- StatefulWidget
   @override
+  // Widget: build methodunun kaitaruu tibi.
+  // (BuildContext context): build methoddunun parametri
   Widget build(BuildContext context) {
+    // return: kaitaryp beruu
+    // Scaffold: bul -> Widget. Ekran kurup beret
     return Scaffold(
+      // appBar: Scaffolddin proportisi
+      // AppBar: bul Widget. tirkemenin shapkasy
       appBar: AppBar(
+        // backgroundColor: AppBardin proportisi. Color alat
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // title: AppBardin proportisi. Widget alat
+        // Text: bul Widget
         title: Text(widget.title),
       ),
+      // body: Scaffold proportisi. Widget alat
+      // Center: bul Widget. centerge koiup beret
       body: Center(
+        // child: Centerdin proportisi. Widget alat
+        // Column: bul Widget. Elementterdii tiginen tizip beret
         child: Column(
+          // mainAxisAlignment: Columndun proportisi. elementterdin ordularyp ozgortup beret
           mainAxisAlignment: MainAxisAlignment.center,
+          // children: Columndun proportisi. Bir neche Widgettterdi ala alat.
+          // <Widget>: generic widgetterdi bere alabiz
           children: <Widget>[
+            // const: ozgorboi torgan nersege berebis
+            // Text: bul Widget
             const Text(
               'You have pushed the button this many times:',
             ),
+            // Text: bul Widget
+            // '$_counter': ozgormonun aty
             Text(
               '$_counter',
+              // style: Text proportisi. TextStyle alat
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
+      // floatingActionButton: Scaffold proportisi. Ozuno Widget alat.
+      // FloatingActionButton: bul Widget. Tegerek knopka beret
       floatingActionButton: FloatingActionButton(
+        // onPressed: FloatingActionButton proportisi. FloatingActionButtondin atkaruusu jazylat
         onPressed: _incrementCounter,
+        // tooltip: FloatingActionButton proportisi. Text chigat
         tooltip: 'Increment',
+        // child: FloatingActionButton proportisi. Widget alat
+        // const: ozgorboi torgan nersege berebis
+        // Icon: bul Widget. Iconka alat
         child: const Icon(Icons.add),
       ),
     );
