92a94972ec0487ac151e81d3d793007010219277
mustafanabiev <nabievvv02@gmail.com>
2023-06-27 00:20:48 +0600

    game_test

---
+++ game_test/lib/constants/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgColor = Color(0xffF0F0F0);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+  static const Color yellow = Color(0xffF09D12);
+  static const Color blue = Color(0xff1400EF);
+  static const Color europe = Color(0xff82A2FD);
+  static const Color asia = Color(0xffFE8181);
+  static const Color northAmerica = Color(0xffFEB100);
+  static const Color southAmerica = Color(0xffE5A9E4);
+  static const Color africa = Color(0xffFEEF35);
+  static const Color australia = Color(0xff6BF782);
+}
