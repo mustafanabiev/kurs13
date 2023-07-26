bb6d8bbd1d1c7e2040de16e780efcd8a2888f9da
mustafanabiev <nabievvv02@gmail.com>
2023-07-27 00:14:52 +0600

    .

---
+++ weather_app2/lib/constants/api_const.dart
@@ -0,0 +1,6 @@
+class ApiConst {
+  // static const String apiKey = '8399a11d4cc690469e5d3c2caa02dc98';
+  // static const String cityName = 'batken';
+  static const String api =
+      'https://api.openweathermap.org/data/2.5/weather?q=osh&appid=8399a11d4cc690469e5d3c2caa02dc98';
+}
