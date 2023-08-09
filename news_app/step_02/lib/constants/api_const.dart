75cd7adcdf90882aaabea4859355cbe05b991d7c
mustafanabiev <nabievvv02@gmail.com>
2023-08-10 00:41:06 +0600

    step_02

---
+++ news_app/step_02/lib/constants/api_const.dart
@@ -0,0 +1,5 @@
+class ApiConst {
+  static const String apiKey = '3548d4bfaff946688fc6cb2fdee369d0';
+  static const String topNews =
+      'https://newsapi.org/v2/top-headlines?country=us&apiKey=$apiKey';
+}
