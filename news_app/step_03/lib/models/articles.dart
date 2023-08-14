3e6b46343dc7b86531b30ca7d1c0c1bb2b13a0a9
mustafanabiev <nabievvv02@gmail.com>
2023-08-14 23:22:33 +0600

    news app 3

---
+++ news_app/step_03/lib/models/articles.dart
@@ -0,0 +1,51 @@
+import 'package:step_03/models/source.dart';
+
+class Articles {
+  const Articles({
+    required this.source,
+    this.author,
+    required this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    required this.publishedAt,
+    this.content,
+  });
+
+  final Source source;
+  final String? author;
+  final String title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String publishedAt;
+  final String? content;
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'source': source.toJson(),
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'urlToImage': urlToImage,
+      'publishedAt': publishedAt,
+      'content': content,
+    };
+  }
+
+  factory Articles.fromJson(Map<String, dynamic> json) {
+    return Articles(
+      source: Source.fromJson(json['source'] as Map<String, dynamic>),
+      author: json['author'] != null ? json['author'] as String : null,
+      title: json['title'] as String,
+      description:
+          json['description'] != null ? json['description'] as String : null,
+      url: json['url'] != null ? json['url'] as String : null,
+      urlToImage:
+          json['urlToImage'] != null ? json['urlToImage'] as String : null,
+      publishedAt: json['publishedAt'] as String,
+      content: json['content'] != null ? json['content'] as String : null,
+    );
+  }
+}
