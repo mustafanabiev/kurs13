cee2651cccba6fa43b188ebcb9d306922693f3c5
Мустафа Набиев <nabievv02@gmail.com>
2023-09-19 00:36:51 +0600

    cubit

---
+++ counter_with_bloc/lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:counter_with_bloc/cubit/counter_cubit.dart';
+import 'package:counter_with_bloc/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: BlocProvider(
+        create: (context) => CounterCubit(),
+        child: const HomePage(),
+      ),
+    );
+  }
+}
