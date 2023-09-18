d4adb35faa6c78aee66c3c3ca6b90dc649a6d571
Мустафа Набиев <nabievv02@gmail.com>
2023-09-18 23:46:14 +0600

    flash chat finished

--- flash_chat/lib/app/app.dart
+++ flash_chat/lib/app/app.dart
@@ -3,8 +3,9 @@ import 'package:flash_chat/modules/welcome/welcome_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp(this.isAuth, {super.key});
+  const MyApp(this.isAuth, this.password, {super.key});
   final bool isAuth;
+  final String password;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: isAuth ? const HomePage() : const WelcomePage(),
+      home: isAuth ? HomePage(password: password) : const WelcomePage(),
     );
   }
 }
